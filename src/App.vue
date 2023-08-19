816b88965c79523b939e1d7f43a9e8fd872468ed
Jacques <hey@jacquescantin.com>
2023-08-19 16:45:29 +1200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Quiz from './components/Quiz.vue';
+import Options from './components/Options.vue';
+import Restart from './components/Restart.vue';
+import { store } from './store';
+
+export default {
+  components: {
+    Quiz,
+    Options,
+    Restart,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="text-gray-700">
+    <Options v-if="store.step == 0"></Options>
+    <Quiz v-if="store.step == 1"></Quiz>
+    <Restart v-if="store.step == 2"></Restart>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: 'Epilogue';
+  background-color: #f9f9f9;
+}
+input,
+textarea,
+button,
+select,
+a {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 }
 </style>
