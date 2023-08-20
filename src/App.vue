884580374686cf0940e84a3a5c51b51bb5e7f649
Jacques <hey@jacquescantin.com>
2023-08-20 19:18:37 +1200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,59 @@
+<template>
+  <div id="app">
+    <h1>Quiz App</h1>
+    <quiz @quiz-completed="handleQuizCompleted" :key="quizKey" />
+    <custom-modal
+      v-show="showModal"
+      header="Congratulations!"
+      subheader="You've completed your Quiz!"
+      :score="score"
+      @reload="updateQuiz"
+      @close="showModal = false"
+    />
+  </div>
+</template>
+
 <script>
-import Quiz from './components/Quiz.vue';
-import Options from './components/Options.vue';
-import Restart from './components/Restart.vue';
-import { store } from './store';
+import CustomModal from "./components/CustomModal.vue";
+import Quiz from "./components/Quiz.vue";
 
 export default {
-  components: {
-    Quiz,
-    Options,
-    Restart,
-  },
+  components: { Quiz, CustomModal },
+  name: "App",
   data() {
     return {
-      store,
+      quizKey: 0,
+      showModal: false,
+      score: {
+        allQuestions: 0,
+        answeredQuestions: 0,
+        correctlyAnsweredQuestions: 0,
+      },
     };
+  },
+  methods: {
+    handleQuizCompleted(score) {
+      this.score = score;
+      this.showModal = true;
+    },
+    updateQuiz() {
+      this.showModal = false;
+      this.quizKey++;
+    },
   },
 };
 </script>
 
-<template>
-  <div class="text-gray-700">
-    <Options v-if="store.step == 0"></Options>
-    <Quiz v-if="store.step == 1"></Quiz>
-    <Restart v-if="store.step == 2"></Restart>
-  </div>
-</template>
-
 <style>
-body {
-  font-family: 'Epilogue';
-  background-color: #f9f9f9;
+* {
+  box-sizing: border-box;
 }
-input,
-textarea,
-button,
-select,
-a {
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  line-height: 1.6;
 }
 </style>
